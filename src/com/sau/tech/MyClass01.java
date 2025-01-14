a278a991f3203e288557acae6ac7570a54802663
veerapatr420 <S6752410008@sau.ac.th>
2025-01-14 14:13:44 +0700

    v.2: create MyClass01, MyClass02 and Edit MyClass00

---
+++ src/com/sau/tech/MyClass01.java
@@ -0,0 +1,10 @@
+package com.sau.tech;
+
+import java.net.SocketOption;
+
+public class MyClass01 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hi World!");
+    }
+}
